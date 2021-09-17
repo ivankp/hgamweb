a782deaa451d7f6afa44a08357c7a11686575531
ivankp <ivan.pogrebnyak@gmail.com>
2021-09-16 22:52:20 -0400

    added tcnt and debug script

---
+++ debug.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+gdb -ex=r --args ./bin/hgamweb '{
+  "H": "h024",
+  "V": "pT_yy",
+  "edges": [ 0, 500 ],
+  "Bf": "exppoly",
+  "Bdeg": 2,
+  "Bdiv": 1,
+  "Sdiv": 1,
+  "nV": 100
+}'
